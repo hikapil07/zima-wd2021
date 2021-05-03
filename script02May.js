5e24c77be1c864cab9281817f5841b50000888ce
Kapil Wadhwa <kapilwadhwa@gmail.com>
2021-05-03 22:16:00 +0530

    adding js and flex sample

---
+++ script02May.js
@@ -0,0 +1,37 @@
+// var namVar, stringVar;
+var var1;
+var txtName = "Ankit";
+console.log(var1);
+var num1 = 10.5;
+var num2 = 12.3;
+var1 = num1 + num2;
+console.log(var1);
+var var2 = 'Hello "prime" user';
+console.log(var2);
+console.log("var2");
+var bol1 = true;
+console.log(bol1);
+console.log(num1 > num2);
+var mySet = ["Ankit", "Pulkit", "Shruti"];
+console.log(mySet);
+
+// alert(var1);
+console.log(document.getElementById('outpuDiv'));
+document.getElementById('outpuDiv').innerHTML = var1;
+document.write(var1);
+console.log("<p>This is Para by JS</p>");
+document.write("<p>This is Para by JS</p>");
+
+var txt1 = "Welcome user";
+var txt2 = " to javascript wrold";
+document.write(txt1 + txt2);
+document.write("<br />");
+document.write("Hi user" + txt2);
+// document.write("<br />");
+document.write("<p>Hi " + txtName + txt2 + "</p>");
+
+
+
+// var myText = document.querySelector("#childDiv").innerText;
+// console.log(myText);
+// console.log("test");
